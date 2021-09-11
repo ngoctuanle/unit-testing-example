2407d29964e077f39f4db06ebbf53f898c0dbe22
Tuan Le <ngoctuanle020395@gmail.com>
2021-09-11 21:16:37 +0700

    initial commit

---
+++ apps/unit-testing-example/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'unit-testing-example-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'unit-testing-example';
+}
